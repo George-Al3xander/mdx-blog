d4a390b8e55555be70ba97dfe5f982bc72240824
George <george.valuiskyi@gmail.com>
2024-06-04 22:12:35 +0300

    feat: add prettier
    Add prettier with tailwind plugin and move rehype plugins array into a
    separate file

--- mdx-options.tsx
+++ mdx-options.tsx
@@ -4,21 +4,24 @@ import rehypePrettyCode from "rehype-pretty-code"
 import { SerializeOptions } from "@/types/types"
 //import { rehypeExtendedTable } from 'rehype-extended-table';
 import gfm from "remark-gfm"
+
+export const rehypePlugins = [
+  //rehypeHighlight,
+  rehypeSlug,
+  //rehypeExtendedTable,
+  gfm,
+  [rehypePrettyCode, { theme: "github-dark" }],
+  [
+    rehypeAutolinkHeadings,
+    {
+      behavior: "wrap",
+    },
+  ],
+]
+
 export const options: SerializeOptions = {
   parseFrontmatter: true,
   mdxOptions: {
-    rehypePlugins: [
-      //rehypeHighlight,
-      rehypeSlug,
-      //rehypeExtendedTable,
-      gfm,
-      [rehypePrettyCode, { theme: "github-dark" }],
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "wrap",
-        },
-      ],
-    ],
+    rehypePlugins,
   },
 }
