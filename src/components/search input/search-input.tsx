067d05e409def258e240db0a750c4ed190987b1f
George <george.valuiskyi@gmail.com>
2024-06-19 10:33:43 +0300

    refactor: add a submit button to the search input
    Add a submit button to the search input
    and change the styles of the reset input button.

--- src/components/search input/search-input.tsx
+++ src/components/search input/search-input.tsx
@@ -6,25 +6,41 @@ import { useSearch } from "@/hooks/useSearch"
 import { Search, X } from "lucide-react"
 
 function SearchInput() {
-  const { search, searchQuery, handleChange, clear } = useSearch()
+  const { search, isValid, searchQuery, handleChange, clear } = useSearch()
   return (
-    <form onSubmit={search} className="w-full">
-      <fieldset className="flex w-full items-center gap-2 rounded-lg border-2 border-black bg-white p-2 dark:border-white dark:bg-black">
+    <form
+      onSubmit={search}
+      className="flex w-full items-center overflow-hidden rounded-lg border-2 border-black bg-white dark:border-white dark:bg-black"
+    >
+      <fieldset className="flex w-full items-center gap-2 p-2">
         <label htmlFor="search-input">
           <Search />
         </label>
         <input
           onChange={handleChange}
           value={searchQuery}
-          placeholder="Search for anyting...!"
+          placeholder="Search for anything...!"
           className="w-[10px] basis-[100%] bg-white outline-none dark:bg-black"
           id="search-input"
           type="text"
         />
-        <Button type="reset" size="sm" onClick={clear}>
+        <Button
+          aria-label="Clear search input"
+          type="reset"
+          size="sm"
+          variant="ghost"
+          onClick={clear}
+        >
           <X />
         </Button>
       </fieldset>
+      <Button
+        className="m-0 h-[52px] rounded-none"
+        disabled={!isValid}
+        type="submit"
+      >
+        Search
+      </Button>
     </form>
   )
 }
