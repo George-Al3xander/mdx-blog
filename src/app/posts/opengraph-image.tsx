d8f22cb34f982c84b35524a730ff517af0485c73
George <george.valuiskyi@gmail.com>
2025-01-06 11:17:54 +0200

    refactor: set og image only in posts root dir
    Remove custom OpenGraph images from the /articles and /programs folders
    and create a general OG image component in the posts root folder.

--- src/app/posts/opengraph-image.tsx
+++ src/app/posts/opengraph-image.tsx
@@ -8,14 +8,15 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
+const POST_TYPE = "all";
 export const size = OG_IMAGE_CONSTS.size;
 export const contentType = OG_IMAGE_CONSTS.contentType;
-export const alt = pagesMetaData.articles.title;
+export const alt = pagesMetaData[POST_TYPE].title;
 
 export default async function Image() {
     const font = await getLocalFont();
 
-    return new ImageResponse(<OgTemplate {...pagesMetaData.articles} />, {
+    return new ImageResponse(<OgTemplate {...pagesMetaData[POST_TYPE]} />, {
         ...size,
         fonts: [font],
     });
